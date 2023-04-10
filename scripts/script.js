4cb27e99be5778768f488fee65ef6253e4b18a12
Naufal Abdurrahman Burhani <40574353+Zeknous@users.noreply.github.com>
2023-04-11 06:28:00 +0700

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  // save 
+  var windowWidth = $(window).width();
+  if (windowWidth < 768) {
+    console.log(screen.width)
+    $('nav').addClass('scrolled');
+  } else {
+    $('.nav-link').addClass('white-text');
+
+    $(window).scroll(function () {
+      // save last scroll top so when page is refreshed the navbar wont changed to transparent if scrolled
+      localStorage.setItem("location", $(this).scrollTop());
+      if (localStorage.getItem("location") > 0) {
+        $('nav').addClass('scrolled');
+        $('.nav-link').removeClass('white-text');
+      } else {
+        $('nav').removeClass('scrolled');
+        $('.nav-link').addClass('white-text');
+
+      }
+
+
+    });
+  }
+
+
+
+
+});
+
+
+
+
